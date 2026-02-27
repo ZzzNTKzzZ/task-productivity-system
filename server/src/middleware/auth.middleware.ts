2a1acc43ab0a734581d0fd48dd50de22dfce8a03
ZzzNTKzzZ <ntk22052006@gmail.com>
2026-02-28 02:15:48 +0700

    Added role middleware and modify auth middleware

--- server/src/middleware/auth.middleware.ts
+++ server/src/middleware/auth.middleware.ts
@@ -1,1 +1,20 @@
 import jwt from "jsonwebtoken"
+import type { Request, Response, NextFunction } from "express"
+import type { MyJwtPayload } from "../types/jwt.js"
+const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.headers.authorization
+
+    if(!authHeader) return res.status(401).json({ message: "No token provider"})
+
+    const token = authHeader.split(" ")[1] || " ";
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET!) as MyJwtPayload
+        req.user = decoded;
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: "Invalid token" });
+    }
+
+}
+
+export default authMiddleware
---
+++ server/src/middleware/role.middleware.ts
@@ -0,0 +1,11 @@
+import type { Request, Response, NextFunction } from "express";
+
+export const roleMiddleware = (requiredRole: string) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        if(!req.user) return res.status(401).json({ message : "Unauthorized"})
+
+        if(req.user.role !== requiredRole) return res.status(403).json({ message: "Forbidden"})
+
+        next()
+    }
+}
