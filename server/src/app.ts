1a059bc89e35007342bc05c3d8562cfbea3029d9
ZzzNTKzzZ <ntk22052006@gmail.com>
2026-03-01 01:38:14 +0700

    Added error handler middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,14 @@
 import dotenv from "dotenv"
 import express from "express";
 import routes from "./routes/index.js";
+import { errorMiddleware } from "./middleware/error.middleware.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json());
-
+app.use(errorMiddleware)
 app.get("/", async (req, res) => {
   res.send("API is working");
 });
