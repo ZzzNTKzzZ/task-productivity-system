d31bbea85b6ee69ce3423f486824b5e34286e832
ZzzNTKzzZ <ntk22052006@gmail.com>
2026-03-02 01:50:20 +0700

    feat: add admin routes and middleware for user routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,12 @@
 import type { Express } from "express";
 import authRouter from "./auth.routes.js";
 import userRouter from "./user.routes.js";
+import adminRouter from "./admin.routes.js";
+import { roleMiddleware } from "../middleware/role.middleware.js";
+import authMiddleware from "../middleware/auth.middleware.js";
 
 export default function routes(app: Express) {
     app.use("/api/auth", authRouter);
-    app.use("/api/user", userRouter)
+    app.use("/api/user", authMiddleware,userRouter)
+    app.use("/api/admin", authMiddleware ,roleMiddleware("ADMIN"), adminRouter)    
 }
