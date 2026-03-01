8a94b9708d9e4c1d2686cd048ecdce2372be4d10
ZzzNTKzzZ <ntk22052006@gmail.com>
2026-03-02 01:33:43 +0700

    feat: add edit user and get profile user

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { UserController } from "../controllers/user.controller.js";
+import authMiddleware from "../middleware/auth.middleware.js";
 
 const userRouter = Router()
 
-userRouter.get("/:id", UserController.getUserById)
+userRouter.get("/profile", authMiddleware, UserController.getCurrentUser)
+userRouter.patch("/editprofile", authMiddleware, UserController.editCurrentUser)
+userRouter.get("/:id" ,authMiddleware ,UserController.getUserById)
 
 export default userRouter
