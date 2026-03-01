4e3f1057dca7fae9c4dd4271a4b53bd674f6440d
ZzzNTKzzZ <ntk22052006@gmail.com>
2026-03-02 01:45:50 +0700

    feat: add get/edit current user and update password

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,3 +1,4 @@
+import { prisma } from "../lib/prisma.js";
 import { UserService } from "../services/user.service.js";
 import type { Response, Request } from "express";
 
@@ -5,19 +6,58 @@ export class UserController {
   // GET: /api/user/:id
   static async getUserById(req: Request, res: Response) {
     try {
-    
-        console.log(req.headers.authorization)
       const id = req.params.id;
 
       if (typeof id !== "string") {
         return res.status(400).json({ message: "Invalid ID" });
       }
 
-      const user = await UserService.User(id);
+      const user = await UserService.getUserById(id);
 
       res.status(200).json(user);
     } catch (error: any) {
       res.status(400).json({ message: error.message });
+    }
+  }
+
+  // GET: /api/user/profile
+  static async getCurrentUser(req: Request, res: Response) {
+    try {
+      const id = req.user?.userId;
+      
+      if (typeof id !== "string") {
+        return res.status(400).json({ message: "Invalid ID" });
+      }
+
+      const user = await UserService.getUserById(id);
+
+      res.status(200).json(user);
+    } catch (error: any) {
+      return res.status(400).json({ message: error.message });
+    }
+  }
+
+  // PATCH: /api/user/editprofile
+  static async editCurrentUser(req: Request, res: Response) {
+    try {
+     const id = req.user?.userId
+     const user = await UserService.updateUser(id!, req.body)
+
+     return res.status(200).json(user)
+    } catch (error: any) {
+      return res.status(404).json({ message: error.message})
+    }
+  }
+
+  // PATCH: /api/user/updatepassword
+  static async updatePassWord(req: Request, res: Response) {
+    try {
+      const id = req.user?.userId
+      const user = await UserService.updatePassword(id!, req.body)
+
+      return res.status(200).json(user)
+    } catch (error: any) {
+      return res.status(404).json({ message: error.message})
     }
   }
 }
