f06a35f950342bdb42aa6426550ff8460a0af946
ZzzNTKzzZ <ntk22052006@gmail.com>
2026-03-02 01:43:54 +0700

    feat: add error middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,17 +4,17 @@ import routes from "./routes/index.js";
 import { errorMiddleware } from "./middleware/error.middleware.js";
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
-app.use(errorMiddleware)
 app.get("/", async (req, res) => {
   res.send("API is working");
 });
 
-
 routes(app)
+
+app.use(errorMiddleware)
 app.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}`);
 });
