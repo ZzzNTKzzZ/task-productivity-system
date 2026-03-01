edbd4db170f2eece506ac5db4c2f95036b4585e6
ZzzNTKzzZ <ntk22052006@gmail.com>
2026-03-02 01:29:52 +0700

    feat: add update user and update password

--- server/src/services/user.service.ts
+++ server/src/services/user.service.ts
@@ -1,8 +1,9 @@
+import bcrypt from "bcrypt"
+import { Prisma } from "@prisma/client";
 import { prisma } from "../lib/prisma.js";
 
 export class UserService {
-  // GET: /api/user/:id
-  static async User(id: string) {
+  static async getUserById(id: string) {
     const user = await prisma.user.findUnique({
       where: { id: id },
     });
@@ -10,5 +11,44 @@ export class UserService {
     if (!user) throw new Error("User not exits");
 
     return user;
+  }
+
+  static async updateUser(
+    id: string,
+    updatePayload: Prisma.UserUncheckedUpdateInput,
+  ) {
+    try {
+      const update = await prisma.user.update({
+        where: { id: id },
+        data: {
+          ...updatePayload,
+        },
+      });
+      return update;
+    } catch (error: any) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === "P2025") {
+          throw new Error(`The user with ID ${id} does not exist.`);
+        }
+      }
+      throw error;
+    }
+  }
+
+  static async updatePassword(id: string, password: string) {
+    try {
+      const hashedPassword = await bcrypt.hash(password, 10)
+
+      const user = await prisma.user.update({
+        where: {id: id},
+        data: {
+          password: hashedPassword
+        }
+      })
+
+      return user
+    } catch (error: any) {
+      return error
+    }
   }
 }
